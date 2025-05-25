10084bd9285b3e097dcc9d81b3a1d9887a5591d8
MarioArocaPaez <mararopae@alum.us.es>
2025-05-25 11:38:10 +0200

    fix: docker config

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,42 +1,21 @@
-import sys
-import os
-from app.models import Data
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app import create_app
+from app.models import Data
 
 def test_home_route():
     app = create_app("development")
     client = app.test_client()
     response = client.get('/data')
     assert response.status_code == 200
-    
+
 def test_root_route():
     app = create_app("development")
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hola mundo" in response.data
-    
-def test_root_route():
-    app = create_app("development")
-    client = app.test_client()
-    response = client.get('/')
-    assert response.status_code == 200
-    assert b"Hola mundo" in response.data
     
 def test_post_data():
     app = create_app("development")
     client = app.test_client()
-
-    response = client.post('/data', json={"name": "TestName"})
-    assert response.status_code == 201
-    assert b"TestName" in response.data
-
-def test_post_data():
-    app = create_app("development")
-    client = app.test_client()
-
     response = client.post('/data', json={"name": "TestName"})
     assert response.status_code == 201
     assert b"TestName" in response.data
@@ -44,7 +23,6 @@ def test_post_data():
 def test_post_data_missing_name():
     app = create_app("development")
     client = app.test_client()
-
     response = client.post('/data', json={})
     assert response.status_code == 400
     assert b"Missing 'name'" in response.data
